32f81876abfdb5f3585377b5ab0217d00306ff0a
luisbarufi <luisbarufi@gmail.com>
2020-04-02 23:12:10 -0300

    added relations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
 
   include SessionsHelper
+
+  private
+
+  def require_logged_in_user
+    unless user_signed_in?
+      flash[:danger] = 'Área restrita. Por favor, realize o login'
+      redirect_to log_in_path
+    end
+  end
 end
