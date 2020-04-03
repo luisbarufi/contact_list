6d4c3ac31c71126cbeb244818cd01331aaba9ca6
luisbarufi <luisbarufi@gmail.com>
2020-04-03 15:30:08 -0300

    fix i18n

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -29,7 +29,7 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
+        format.html { redirect_to @contact, notice: 'Contato criado com sucesso.' }
         format.json { render :show, status: :created, location: @contact }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class ContactsController < ApplicationController
   def update
     respond_to do |format|
       if @contact.update(contact_params)
-        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
+        format.html { redirect_to @contact, notice: 'Contato alterado com sucesso.' }
         format.json { render :show, status: :ok, location: @contact }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class ContactsController < ApplicationController
   def destroy
     @contact.destroy
     respond_to do |format|
-      format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
+      format.html { redirect_to contacts_url, notice: 'Contato excluido com sucesso.' }
       format.json { head :no_content }
     end
   end
