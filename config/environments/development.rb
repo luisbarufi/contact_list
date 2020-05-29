b53470e44e210d4cfee70eda9759588d6412f968
luisbarufi <luisbarufi@gmail.com>
2020-05-29 10:52:18 -0300

    added PWA

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,11 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Whitelist URLs for Ngrok
+  Rails.application.configure  do
+    config.hosts.clear
+  end
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
